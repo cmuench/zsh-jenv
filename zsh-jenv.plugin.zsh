c745346f2994318c2376db43510cfed960da43fa
Christian Münch <christian@muench-worms.de>
2024-06-13 12:54:01 +0200

    Initial commit (WIP)

---
+++ zsh-jenv.plugin.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+# shellcheck disable=SC1090
+#
+
+# See: https://github.com/jenv/jenv
+
+if [ ! -d "$HOME/.jenv" ]; then
+    git clone https://github.com/jenv/jenv.git $HOME/.jenv
+fi
+
+PATH="$HOME/.jenv/bin"
+
+# Activate jEnv
+eval "$(jenv init -)"
+jenv enable-plugin export
+
